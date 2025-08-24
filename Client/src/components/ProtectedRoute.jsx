08bdc6ed2c85b77838ea7ed84b428fc038a955c7
Vaibhav092 <vaibhavcr7092@gmail.com>
2025-08-24 16:53:56 +0530

    Fix bugs

--- Client/src/components/ProtectedRoute.jsx
+++ Client/src/components/ProtectedRoute.jsx
@@ -4,9 +4,6 @@ import { useAuth } from '../context/AuthContext';
 const ProtectedRoute = ({ children }) => {
     const { user, loading } = useAuth();
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
 
     if (!user) {
         return <Navigate to="/login" replace />;
