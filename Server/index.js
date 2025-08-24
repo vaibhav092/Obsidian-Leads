541dee0dc5d5784dd6e446c5154174cc11040f5f
Vaibhav092 <vaibhavcr7092@gmail.com>
2025-08-24 17:59:23 +0530

    Deploy fix

--- Server/index.js
+++ Server/index.js
@@ -18,16 +18,10 @@ const allowedOrigins = [
 
 app.use(
     cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
+        origin: allowedOrigins,
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'X-Requested-With'],
     })
 );
 app.use(express.json());
